bb5c7aab929bb3771dc212596bfa9a77dbf584db
bahir <boudoumabahir@gmail.com>
2022-06-10 09:31:05 +0200

    bahir v2

--- modele/modele/settings.py
+++ modele/modele/settings.py
@@ -75,10 +75,12 @@ WSGI_APPLICATION = 'modele.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'ENGINE': 'django.db.backends.mysql',
+        'NAME':'drive',
+        'HOST':'127.0.0.1',
+        'USER':'toto',
+        'PASSWORD':'toto',
+        'PORT':3306
 }
 
 
